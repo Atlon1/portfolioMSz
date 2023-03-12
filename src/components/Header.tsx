e1392eae951029470fd28cb5bae4504cafd192cc
Atlon1 <maciej.szajstek@gmail.com>
2023-03-12 21:46:44 +0100

    section header rdy

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import Logo from '../assets/Maciej_logo.png'
 
 const Header = () => {
-    return <div className='section' id='home'>Header</div>;
+    return <header id='home' className='py-2'>
+        <div className='container mx-auto'>
+            <div className='flex justify-between items-center '>
+                <a href='#' >
+                    <img className='max-w-[250px] w-full' src={Logo} alt='' />
+                </a>
+                <button className='btn btn-sm'>Pracuj ze mna</button>
+            </div>
+        </div>
+    </header>;
 };
 
 export default Header;
