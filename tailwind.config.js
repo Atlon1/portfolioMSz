88cee258f24e9522eda3c8715b927a58a83d6615
Atlon1 <maciej.szajstek@gmail.com>
2023-03-21 22:40:55 +0100

    color rdy

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,10 +21,10 @@ module.exports = {
     extend: {
       colors: {
         primary: '#0a0a0a',
-        accent: '#B809C3',
+        accent: '#43C6AC',
       },
       backgroundImage: {
-        site: "url('../assets/site-bg.jpg')",
+        site: "url('../assets/bg-site2.jpg')",
         about: "url('../assets/about.png')",
         services: "url('../assets/services.png')",
       },
